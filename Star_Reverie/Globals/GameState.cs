fa33f5b7a33ef82a24454d14b854fe46f17ce9d4
Trenton Scott <InsaneGriot@Outlook.com>
2025-06-29 23:17:44 -0400

    GameState

---
+++ Star_Reverie/Globals/GameState.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Star_Reverie.Globals
+{
+    public enum GameState
+    {
+        Paused,
+        Encounter,
+        Exploration,
+        Dialogue,
+        Scene
+    }
+
+    public static class CurrentGameState
+    {
+        public static GameState GameState = GameState.Exploration;
+    }
+}
