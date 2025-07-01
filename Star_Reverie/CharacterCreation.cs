d76fe6c75cb68501f989720ece608bc3d2f3d5ca
Trenton Scott <InsaneGriot@Outlook.com>
2025-07-01 13:26:43 -0400

    UI CharacterCreation

---
+++ Star_Reverie/CharacterCreation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+
+namespace Star_Reverie
+{
+    public class CharacterCreation : SyncScript
+    {
+        public UIPage CharacterCreationMain;
+
+        public override void Start()
+        {
+            // Initialization of the script.
+        }
+
+        public override void Update()
+        {
+            // Do stuff every new frame
+        }
+    }
+}
