b85f3c46a3e4871846d3301ab2e1163f3846c0ed
Trenton Scott <InsaneGriot@Outlook.com>
2025-07-01 17:47:23 -0400

    Add Strength

--- Star_Reverie/CharacterCreation.cs
+++ Star_Reverie/CharacterCreation.cs
@@ -3,6 +3,7 @@ using Stride.Engine;
 using Stride.UI.Controls;
 using Stride.UI;
 using Stride.UI.Events;
+using System;
 
 namespace Star_Reverie
 {
@@ -16,6 +17,9 @@ namespace Star_Reverie
         //TextBlocks
         TextBlock characterPoints;
         TextBlock ageNumber;
+        TextBlock strengthNumber;
+        TextBlock hitPointsNumber;
+        TextBlock basicLiftNumber;
         public override void Start()
         {
             uiEntity = new();
@@ -40,16 +44,26 @@ namespace Star_Reverie
         }
         private void StatNumberMapping()
         {
-            characterPoints = CharacterCreationMain.RootElement.FindVisualChildOfType<TextBlock>("CharacterPoints");
+            characterPoints = CharacterCreationMain.RootElement.FindVisualChildOfType<TextBlock>("CharacterPointsNumber");
             ageNumber = CharacterCreationMain.RootElement.FindVisualChildOfType<TextBlock>("AgeNumber");
+            strengthNumber = CharacterCreationMain.RootElement.FindVisualChildOfType<TextBlock>("StrengthNumber");
+            hitPointsNumber = CharacterCreationMain.RootElement.FindVisualChildOfType<TextBlock>("HitPointsNumber");
+            basicLiftNumber = CharacterCreationMain.RootElement.FindVisualChildOfType<TextBlock>("BasicLiftNumber");
         }
         private void ButtonEventMapping()
         {
             Button addAge = CharacterCreationMain.RootElement.FindVisualChildOfType<Button>("AddAge");
             Button subtractAge = CharacterCreationMain.RootElement.FindVisualChildOfType<Button>("SubtractAge");
+            Button addStrength = CharacterCreationMain.RootElement.FindVisualChildOfType<Button>("AddStrength");
+            Button subtractStrength = CharacterCreationMain.RootElement.FindVisualChildOfType<Button>("SubtractStrength");
 
             addAge.Click += (object sender, RoutedEventArgs e) => ChangeAge(true);
+            subtractAge.Click += (object sender, RoutedEventArgs e) => ChangeAge(false);
+            addStrength.Click += (object sender, RoutedEventArgs e) => ChangeAttribute("Strength", true);
+            subtractStrength.Click += (object sender, RoutedEventArgs e) => ChangeAttribute("Strength", false);
+            
         }
+
 
         private void ChangeAge(bool add)
         {
@@ -64,6 +78,39 @@ namespace Star_Reverie
                 currentAge -= 1;
             }
             ageNumber.Text = currentAge.ToString();
+        }
+
+        private void ChangeAttribute(string attribute, bool add)
+        {
+            int currentCharacterPoints = int.Parse(characterPoints.Text);
+            int currentStrength = int.Parse(strengthNumber.Text);
+            int currentHitPoints = int.Parse(hitPointsNumber.Text);
+            int currentBasicLift = int.Parse(basicLiftNumber.Text);
+            switch (attribute)
+            {
+                case "Strength":
+                    if (currentCharacterPoints > 0 && add)
+                    {
+                        currentStrength += 1;
+                        currentHitPoints += 1;
+                        currentBasicLift = (currentStrength * currentStrength) / 5;
+                        currentCharacterPoints -= 10;
+                        
+
+                    }
+                    else if (currentCharacterPoints > 0 && !add && currentStrength > 1)
+                    {
+                        currentStrength -= 1;
+                        currentHitPoints -= 1;
+                        currentBasicLift = (currentStrength * currentStrength) / 5;
+                        currentCharacterPoints += 10;
+                    }
+                    strengthNumber.Text = currentStrength.ToString();
+                    characterPoints.Text = currentCharacterPoints.ToString();
+                    hitPointsNumber.Text = currentHitPoints.ToString();
+                    basicLiftNumber.Text = currentBasicLift.ToString();
+                    break;
+            }
         }
     }
 }
