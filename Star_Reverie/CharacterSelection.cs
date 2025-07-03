5972f09d0b2d456d5f992d4585c08c116398e6b1
Trenton Scott <InsaneGriot@Outlook.com>
2025-07-03 13:50:53 -0400

    Rendering LIst

--- Star_Reverie/CharacterSelection.cs
+++ Star_Reverie/CharacterSelection.cs
@@ -6,21 +6,64 @@ using System.Threading.Tasks;
 using Stride.Core.Mathematics;
 using Stride.Input;
 using Stride.Engine;
+using Stride.UI.Controls;
+using StarReverieCore;
+using StarReverieCore.Models;
+using Stride.UI;
+using Stride.UI.Panels;
+using Stride.Graphics;
 
 namespace Star_Reverie
 {
     public class CharacterSelection : SyncScript
     {
-        // Declared public member fields and properties will show in the game studio
+        public UIPage CharacterSelectionPage;
+        private Entity uiEntity;
+        private UIComponent uIComponent;
+        StarReverieDbContext starReverieDbContext = new StarReverieDbContext();
 
+
+        private bool OnScreen;
+        private List<Character> characters;
+        private StackPanel characterList;
         public override void Start()
         {
-            // Initialization of the script.
+            uiEntity = new();
+            uIComponent = new();
+            uIComponent.Page = CharacterSelectionPage;
+            uiEntity.Add(uIComponent);
+
+            characters = starReverieDbContext.Characters.ToList();
+            characterList = CharacterSelectionPage.RootElement.FindVisualChildOfType<StackPanel>("CharacterList");
+            foreach (Character character in characters)
+            {
+                TextBlock textBlock = new TextBlock
+                {
+                    Name = character.FirstName,
+
+                        Text = $"{character.FirstName} {character.LastName}",
+                        Font = Content.Load<SpriteFont>("StrideDefaultFont"),
+                        TextSize = 20,
+                        TextColor = Color.White,
+                        OutlineColor = Color.Black,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                };
+                characterList.Children.Add(textBlock);
+            }
+  
         }
 
         public override void Update()
         {
-            // Do stuff every new frame
+            if (Input.IsKeyPressed(Keys.L))
+            {
+                if (!OnScreen)
+                {
+                    SceneSystem.SceneInstance.RootScene.Entities.Add(uiEntity);
+                    OnScreen = true;
+                }
+
+            }
         }
     }
 }
