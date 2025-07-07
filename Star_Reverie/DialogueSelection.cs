1fc7081442a2e5805791743975a2acb94fb4132d
Trenton Scott <InsaneGriot@Outlook.com>
2025-07-06 20:45:01 -0400

    Dialogue List

--- Star_Reverie/DialogueSelection.cs
+++ Star_Reverie/DialogueSelection.cs
@@ -8,6 +8,13 @@ using Stride.Input;
 using Stride.Engine;
 using System.Security.Principal;
 using Star_Reverie.Globals;
+using StarReverieCore.Models;
+using StarReverieCore;
+using System.Windows.Media.TextFormatting;
+using Stride.UI.Panels;
+using Stride.UI;
+using Stride.UI.Controls;
+using Stride.UI.Events;
 
 namespace Star_Reverie
 {
@@ -22,6 +29,8 @@ namespace Star_Reverie
         private UIComponent UIComponent;
         private bool DialoguePageOnScreen;
         private bool DialgoueResponseOnScreen;
+        private List<DialogueModel> dialogues;
+        private StackPanel dialgoueList;
         public override void Start()
         {
             // Initialization of the script.
@@ -39,11 +48,33 @@ namespace Star_Reverie
 
             if (!DialoguePageOnScreen)
             {
+                StarReverieDbContext starReverieDbContext = new StarReverieDbContext();
                 UIEntity = new();
                 UIComponent = new UIComponent { Page = DialogueSelectionPage };
                 UIEntity.Add(UIComponent);
+
+                dialogues = starReverieDbContext.Dialogues
+                    .ToList();
+
+                dialgoueList = DialogueSelectionPage.RootElement.FindVisualChildOfType<StackPanel>("DialogueList");
+                dialgoueList.Children.Clear();
+                foreach (DialogueModel dialgoue in dialogues)
+                {
+                    Button button = (Button)DialogueSelectionButton
+                        .Instantiate()
+                        .First()
+                        .Get<UIComponent>().Page
+                        .RootElement;
+                    button.Name = dialgoue.SpeakerFirstName;
+                    button.FindVisualChildOfType<TextBlock>().Text = $"{dialgoue.SpeakerFirstName} {dialgoue.SpeakerLastName}";
+                    button.HorizontalAlignment = HorizontalAlignment.Center;
+                    dialgoueList.Children.Add(button);
+
+                   // button.Click += (object sender, RoutedEventArgs e) => ReadCharacter(character);
+                }
                 CurrentGameState.GameState = GameState.Dialogue;
                 DialoguePageOnScreen = true;
+                SceneSystem.SceneInstance.RootScene.Entities.Add(UIEntity);
             }
         }
 
